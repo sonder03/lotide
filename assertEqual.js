f030280484d621e4ff64f3167c6e257917e5a3d6
sonder03 <ravneet.virk@gmail.com>
2023-02-03 22:55:08 -0500

    Saveassertions

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+//FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed:" + actual + "===" + expected);
+
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: " + actual + "!=" + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Assert", "Assert");
+assertEqual("time", "year");
+assertEqual(100, 101);
