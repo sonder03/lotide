256974683f6d4362c432b0e8b5758c97b008c3a3
sonder03 <ravneet.virk@gmail.com>
2023-02-07 19:40:54 -0500

    eqArray revised code

--- eqArray.js
+++ eqArray.js
@@ -1,13 +1,36 @@
-const eqArrays  = function([a1], [a2]) {
-  if (a1 === a2) {
-    console.log(`✅✅✅ Assertion Passed: ${a1} === ${a2}`);  
+const assertEqual1 = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);  
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${a1} != ${a2}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} != ${expected}`);
   }
 };
 
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
+// TEST CODE
+assertEqual1("Lighthouse Labs", "Bootcamp");
+assertEqual1(1, 1);
+assertEqual1("Assert", "Assert");
+assertEqual1("time", "year");
+assertEqual1(100, 101);
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+const eqArrays = function (a1, a2) {
+
+  for (i = 0; i < a1.length; i++) {
+
+    if (a1[i] !== a2[i]) {
+     return false;
+    }
+  }
+
+
+  return true
+};
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+
+assertEqual1(eqArrays([1, 2, 3], [1, 6, 3]), true); // => should PASS
