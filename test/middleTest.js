b72368a44d787790b352824e17a404050d3be880
sonder03 <ravneet.virk@gmail.com>
2023-02-18 21:06:43 -0500

    middle & tail

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,38 @@
-const middle = require('../middle');
 
-console.log(middle([1])) // => []
-console.log(middle([1, 2]))// => []
-console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+
+const assert = require('chai').assert;
+const middle   = require('../middle');
+
+describe("#middle", () => {
+  it("returns 3 for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), 3);
+  });
+
+  it("returns '2.5' for ['1, 2, 3, 4, 5']", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), 2.5); 
+  });
+
+  it("returns [] for ['1, 2']", () => {
+    assert.deepEqual(middle([1, 2]), []); 
+  });
+
+   it("returns [] for ['1']", () => {
+    assert.deepEqual(middle([1]), []); 
+  });
+});
+
+
+
+
+
+
+
+
+// const middle = require('../middle');
+
+// console.log(middle([1])) // => []
+// console.log(middle([1, 2]))// => []
+// console.log(middle([1, 2, 3])) // => [2]
+// console.log(middle([1, 2, 3, 4, 5])); // => [3]
+// console.log(middle([1, 2, 3, 4])) // => [2, 3]
+// console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,25 @@
+const assert = require('chai').assert;
+const tail   = require('../tail');
 
-const assertEqual = require('../assertEqual');
-const tail = require('../tail');
+describe("#tail", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
 
-//TEST CODE
-assertEqual(tail([5,6,7]), [6,7]);
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Helloddd");
+  it("returns '6, 7' for [5, 6, 7]", () => {
+    assert.deepEqual(tail([5,6,7]), [6,7]); 
+  });
+
+
+});
+
+
+
+
+// const assertEqual = require('../assertEqual');
+// const tail = require('../tail');
+
+// //TEST CODE
+// assertEqual(tail([5,6,7]), [6,7]);
+// assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Helloddd");
