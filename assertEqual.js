558021d963b9f433c8d5fbc6ffe9c51bba31c66d
sonder03 <ravneet.virk@gmail.com>
2023-02-18 19:28:34 -0500

    assertEqualTest.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,4 @@
-const assertEqual1 = function(actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -6,35 +6,9 @@ const assertEqual1 = function(actual, expected) {
   }
 };
 
-// "assertEqual.js"// //FUNCTION IMPLEMENTATION
-// const assertEqual1 = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log("✅✅✅ Assertion HAS Passed:" + actual + "===" + expected);
 
-//   } else {
-//     console.log("🛑🛑🛑 Assertion HAS Failed: " + actual + "!=" + expected);
-//   }
-// };
 
-// TEST CODE
-assertEqual1("Lighthouse Labs", "Bootcamp");
-assertEqual1(1, 1);
-assertEqual1("Assert", "Assert");
-assertEqual1("time", "year");
-assertEqual1(100, 101);
 
-// //INTERPOLATION STRING
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);  
-//   } else {
-//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} != ${expected}`);
-//   }
-// };
 
-// // TEST CODE
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-// assertEqual("Assert", "Assert");
-// assertEqual("time", "year");
-// assertEqual(100, 101);
+
+module.exports = assertEqual;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Assert", "Assert");
+assertEqual("time", "year");
+assertEqual(100, 101);
